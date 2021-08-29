d6f3e497a6b9176071c91a255a9182e637477f03
Forejy <joffrey.marques@gmail.com>
2021-08-29 04:02:34 +0200

    Tutorial terminé

--- src/index.js
+++ src/index.js
@@ -1,45 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { unstable_concurrentAct } from 'react-dom/cjs/react-dom-test-utils.production.min';
 import './index.css';
 
 function Square(props) {
   return (
     <button className="square" onClick={ props.onClick }>
-      {props.value}
+      { props.value }
     </button>
   );
 }
 
 class Board extends React.Component {
   renderSquare(i) {
+    let propsSquare = this.props.squares[i];
+    let printedSquare;
+    const winnerSquares = calculateWinner(this.props.squares);
+
+    printedSquare = winnerSquares && winnerSquares.includes(i) ? <span style={{color: "red"}}>{propsSquare}</span> : propsSquare;
+
     return (
       <Square
-        value={ this.props.squares[i] } // Vous venez de « passer une prop » d’un composant parent Board à un composant enfant Square. Dans les applis React, c’est grâce au passage de props que l’information circule, toujours des parents vers les enfants.
-        onClick={ () => this.props.onClick(i) } /> //On passe une fonction de board à Square, parce que Square ne peut pas accéder aux propriétés de Board (étant privées)
+        key={i}
+        value={ printedSquare }
+        onClick={ () => this.props.onClick(i) } />
     );
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+    let rows = [];
+    for (let i = 0; i < 3; i++) {
+
+      let currentRow = [];
+      for (let j = 0; j < 3; j++) {
+        currentRow.push(this.renderSquare((i * 3) + j));
+      }
+
+      rows.push(
+        <div
+        key={i}
+         className="board-row">
+          {currentRow}
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+      )
+    }
+
+    return(rows)
   }
 }
 
@@ -47,8 +52,9 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{ //history est une Array d'Objets, c'est comme les Hashs de Ruby.
+      history: [{
         squares: Array(9).fill(null),
+        moves: [],
       }],
       stepNumber: 0,
       xIsNext: true,
@@ -59,16 +65,19 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
+    const moves = current.moves.slice();
 
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     else {
       squares[i] = this.state.xIsNext ? 'X' : 'O';
+      moves.push([i % 3, Math.floor(i / 3)]);
     }
     this.setState({
       history: history.concat([{
         squares: squares,
+        moves: moves
     }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext });
@@ -84,27 +93,36 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
+
     const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ?
-      'Revenir au tour n°' + move :
-      'Revenir au début de la partie';
-      return (
-        <li key={ move }>
-          <button onClick={ () => this.jumpTo(move) }>{ desc }</button>
-        </li>
-      )
-    })
-
     let status;
+
     if (winner) {
-      status = winner + " a gagné";
+      status = (this.state.xIsNext ? 'O' : 'X') + " a gagné";
+    }
+    else if (history.length === 10 && !winner) {
+      status = <b>"It's a Draw"</b>;
     }
     else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
     }
 
+    const moves = history.map((step, move) => {
+      let desc, part1, part2;
+      if (move) {
+        desc = (move === this.state.stepNumber) ? (<b>{move}</b>) : move
+        part1 = 'Revenir au tour n°'
+        part2 = ' - Mouvement [' + step.moves.slice(-1) + ']'
+      }
+      else {
+        desc = 'Revenir au début de la partie';
+      }
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{part1}{desc}{part2}</button>
+        </li>
+      );
+    })
 
     return (
       <div className="game">
@@ -137,7 +155,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return lines[i];
     }
   }
   return null
